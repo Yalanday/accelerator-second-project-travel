0f69934f08f1609c4bf4f4b53ede29108f4cbcf0
Yalanday <oeb72umvd@mial.ru>
2024-04-21 03:44:18 +0300

    added style & js section tours

--- source/js/hero-swiper.js
+++ source/js/hero-swiper.js
@@ -1,11 +1,11 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 
 export const heroSwiper = new Swiper('.hero__swiper', {
-  modules: [Navigation, Pagination],
+  modules: [Pagination],
   slidesPerView: 1,
   spaceBetween: 0,
   loop: true,
