87ad099afe12a8ae51a9c07cde4494a78d2f7112
Yalanday <yalanday256@gmail.com>
2024-04-21 20:45:18 +0300

    added style js section reviews

---
+++ source/js/reviews-swiper.js
@@ -0,0 +1,30 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/bundle';
+
+
+export const toursSwiper = new Swiper('.reviews__swiper', {
+  modules: [Navigation],
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    },
+    768: {
+      slidesPerView: 1.19,
+      spaceBetween: 30
+    },
+    1440: {
+      slidesPerView: 1.6,
+      spaceBetween: 120
+    }
+  },
+  navigation: {
+    nextEl: '.reviews__swiper-button-next',
+    prevEl: '.reviews__swiper-button-prev',
+  },
+
+});
