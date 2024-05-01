c777c7ea2f07feb125ec4de9539b969760c3f64e
Yalanday <oeb72umvd@mial.ru>
2024-05-01 19:58:48 +0300

    finish test pp header, hero

--- pp.config.cjs
+++ pp.config.cjs
@@ -1,11 +1,11 @@
 const desktopSections = [
   // {section: 'header', misMatchThreshold: 1.1},
-  // {section: 'hero', misMatchThreshold: 0.4},
+  {section: 'hero', misMatchThreshold: 0.4},
   // {section: 'tours', misMatchThreshold: 0.7},
   // {section: 'training', misMatchThreshold: 2.1},
   // {section: 'about', misMatchThreshold: 1.5},
   // {section: 'reviews', misMatchThreshold: 2.4},
-  {section: 'adv', misMatchThreshold: 1.2},
+  // {section: 'adv', misMatchThreshold: 1.2},
   // {section: 'gallery', misMatchThreshold: 0.4},
   // {section: 'form', misMatchThreshold: 0.6},
   // {section: 'footer', misMatchThreshold: 1.2}
@@ -13,7 +13,7 @@ const desktopSections = [
 
 const tabletSections = [
   // {section: 'header', misMatchThreshold: 1.3},
-  // {section: 'hero', misMatchThreshold: 0.8},
+  {section: 'hero', misMatchThreshold: 0.8},
   // {section: 'tours', misMatchThreshold: 1.1},
   // {section: 'training', misMatchThreshold: 3.5},
   // {section: 'about', misMatchThreshold: 3.5},
@@ -26,7 +26,7 @@ const tabletSections = [
 
 const mobileSections = [
   // {section: 'header', misMatchThreshold: 2.2},
-  // {section: 'hero', misMatchThreshold: 1.2},
+  {section: 'hero', misMatchThreshold: 1.2},
   // {section: 'tours', misMatchThreshold: 2.2},
   // {section: 'training', misMatchThreshold: 4.3},
   // {section: 'about', misMatchThreshold: 3.5},
